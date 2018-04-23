fd93a84b4d0de0747a65d7c23821d0f4c6063005
Benjamin Liden <benjamin.c.liden@gmail.com>
2018-04-23 11:16:40 -0400

    title card roughed in + transition. first /docs push for github pages

---
+++ src/scripts/titleAnimation.js
@@ -0,0 +1,9 @@
+let titleCard = document.querySelector('.title-card');
+
+document.addEventListener("DOMContentLoaded", function(){
+    console.log('hello?');
+    document.body.style.visibility = 'visible';
+    titleCard.classList.remove('is-transitioning');
+});
+
+console.log('hi mom!');
