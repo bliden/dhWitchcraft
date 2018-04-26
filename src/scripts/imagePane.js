d77f1f24261a3353b6550089a45c4d6d369d9ad4
Benjamin Liden <benjamin.c.liden@gmail.com>
2018-04-26 13:05:09 -0400

    all Scatterplots loaded. Method link working. title transition slimmed.

--- src/scripts/imagePane.js
+++ src/scripts/imagePane.js
@@ -4,22 +4,44 @@ const imagePane = document.getElementById('imagePane');
 
 // load images here to be paired with text in the scroller
 
+const dir = "assets/graphs/";
+
 const imageArray = [
     {
-        src: "assets/graphs/Scatterplot_SnowWhite.png",
-        zoom: "assets/graphs/Scatterplot_SnowWhite.png",
+        src: dir + "Scatterplot_SnowWhite.png",
+        zoom: dir + "Scatterplot_SnowWhite.png",
         alt: "Scatterplot of Snow White"
     },
     {
-        src: "assets/graphs/Scatterplot_BlairWitchProject.png",
-        zoom: "assets/graphs/Scatterplot_BlairWitchProject.png",
+        src: dir + "Scatterplot_BlairWitchProject.png",
+        zoom: dir + "Scatterplot_BlairWitchProject.png",
         alt: "Scatterplot of Blair Witch Project"
     },
     {
-        src: "assets/graphs/Scatterplot_HarryPotter.png",
-        zoom: "assets/graphs/Scatterplot_HarryPotter.png",
+        src: dir + "Scatterplot_HarryPotter.png",
+        zoom: dir + "Scatterplot_HarryPotter.png",
         alt: "Scatterplot of Harry Potter"
-    }
+    },
+    {
+        src: dir + "Scatterplot_Halloweentown.png",
+        zoom: dir + "Scatterplot_Halloweentown.png",
+        alt: "Scatterplot of Halloweentown",
+    },
+    {
+        src: dir + "Scatterplot_TheCraft.png",
+        zoom: dir + "Scatterplot_TheCraft.png",
+        alt: "Scatterplot of Witches of Eastwick",
+    },
+    {
+        src: dir + "Scatterplot_WitchesOfEastwick.png",
+        zoom: dir + "Scatterplot_WitchesOfEastwick.png",
+        alt: "Scatterplot of Witches of Eastwick",
+    },
+    // {
+    //     src: "",
+    //     zoom: "",
+    //     alt: "",
+    // },
 ]
 
 export { imagePane, imageArray };
