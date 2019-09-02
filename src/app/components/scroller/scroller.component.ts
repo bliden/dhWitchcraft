f5bdd2111512de43d5bf442ae6e8d5a4690b81a2
Benjamin Liden <benjamin.c.liden@gmail.com>
2019-09-02 10:52:23 -0400

    scrollytelling working. need background, but it's progress

--- src/app/components/scroller/scroller.component.ts
+++ src/app/components/scroller/scroller.component.ts
@@ -5,12 +5,11 @@ import {
   ElementRef,
   AfterViewInit,
   ViewChildren,
-  QueryList,
   HostListener
 } from "@angular/core";
 import { BreakpointObserver, Breakpoints } from "@angular/cdk/layout";
-import { map } from "rxjs/operators";
-import { Observable } from "rxjs";
+import { map, debounce, debounceTime } from "rxjs/operators";
+import { Observable, fromEvent, Subscription } from "rxjs";
 import scrollama from "scrollama";
 import { DriftzoomDirective } from "src/app/directives/driftzoom.directive";
 
@@ -25,16 +24,23 @@ export class ScrollerComponent implements OnInit, AfterViewInit {
     .observe([Breakpoints.XSmall, Breakpoints.Small])
     .pipe(map(result => result.matches));
 
+  @ViewChild("scrollContainer", { static: true })
+  scrollContainerRef: ElementRef;
+  @ViewChild("scrollText", { static: true }) scrollTextRef: ElementRef;
   @ViewChild("scrollGraphic", { static: true }) scrollGraphicRef: ElementRef;
+  @ViewChild("chart", { static: true }) chartRef: ElementRef;
   @ViewChildren("step") steps: any;
   @ViewChildren(DriftzoomDirective) images: any;
   stepRefs: any[];
   imageRefs: any[];
+  scroller: any;
+  resizeListener: Subscription;
 
   // listener for window resize. use to redraw scroller
   @HostListener("window:resize", ["$event"])
   onResize(event) {
     console.log(event.target.innerWidth, event.target.innerHeight);
+    this.handleResize();
   }
 
   constructor(private breakpointObserver: BreakpointObserver) {}
@@ -44,6 +50,58 @@ export class ScrollerComponent implements OnInit, AfterViewInit {
   ngAfterViewInit() {
     this.buildRefArrays();
     this.initScrollama();
+    this.windowResizeListener();
+  }
+
+  handleResize() {
+    const graphic = this.scrollGraphicRef.nativeElement;
+    const text = this.scrollTextRef.nativeElement;
+    const chart = this.chartRef.nativeElement;
+
+    // 1. update height of step elems for room between steps
+
+    const stepHeight = Math.floor(window.innerHeight * 0.25);
+    this.stepRefs.forEach(function(singleStep) {
+      singleStep.style.marginBottom = `${stepHeight}px`;
+    });
+
+    // 2. update height of graphic elem
+
+    graphic.style.height = window.innerHeight + "px";
+
+    // 3. update width of chart by subrtacting from text width
+
+    const textWidth = text.offsetWidth;
+    const bodyWidth = document.body.offsetWidth;
+    const chartWidth = bodyWidth - textWidth - 32;
+
+    // make the height of 1/2 of viewport
+    const chartHeight = Math.floor(window.innerHeight * 0.95);
+
+    chart.style.width = chartWidth + "px";
+    chart.style.height = chartHeight + "px";
+
+    // zoom factor recalced on resize. smaller screen > larger zoom
+    // driftSet.forEach(function(drift) {
+    //   if (drift.triggerEl.classList.contains("boxOffice")) {
+    //     drift.zoomFactor = (3.5 / bodyWidth) * 10 ** 3;
+    //     // console.log(drift.zoomFactor);
+    //   } else if (drift.triggerEl.classList.contains("imageplot")) {
+    //     drift.zoomFactor = (6 / bodyWidth) * 10 ** 3;
+    //     // console.log(drift.zoomFactor);
+    //   } else {
+    //     drift.zoomFactor = (4 / bodyWidth) * 10 ** 3;
+    //   }
+    // });
+
+    // 4. tell scrollama to update new elem dmensions
+    this.scroller.resize();
+  }
+
+  windowResizeListener() {
+    this.resizeListener = fromEvent(window, "resize")
+      .pipe(debounceTime(500))
+      .subscribe(this.onResize);
   }
 
   /*
@@ -60,10 +118,14 @@ export class ScrollerComponent implements OnInit, AfterViewInit {
     setup scroller and define utility functions
   */
   initScrollama() {
-    const scroller = scrollama();
-    scroller
+    this.scroller = scrollama();
+    this.scroller
       .setup({
-        step: this.stepRefs
+        container: this.scrollContainerRef.nativeElement, // outermost element
+        graphic: this.scrollGraphicRef.nativeElement, // the graphic
+        text: this.scrollTextRef.nativeElement, // the step container
+        step: this.stepRefs, // the steps
+        offset: 0.7
       })
       .onStepEnter(response => {
         const { element, index, direction } = response;
@@ -80,9 +142,24 @@ export class ScrollerComponent implements OnInit, AfterViewInit {
           }
         });
       })
-      .onStepExit(response => {
-        // { element, index, direction }
-        // console.log(response);
+      .onContainerEnter(response => {
+        console.log("!!!", response);
+        const graphic = this.scrollGraphicRef.nativeElement;
+        graphic.classList.add("is-fixed");
+        graphic.classList.remove("is-bottom");
+      })
+      .onContainerExit(response => {
+        console.log("!!!", response);
+        const graphic = this.scrollGraphicRef.nativeElement;
+        graphic.classList.remove("is-fixed");
+        if (response.direction === "down") {
+          graphic.classList.add("is-bottom");
+        } else {
+          graphic.classList.remove("is-bottom");
+        }
       });
+
+    // resize only AFTER scrollama is initialized
+    this.handleResize();
   }
 }
